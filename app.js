ad66fa77cb46e94983fae079d7692104d10dbcd0
Semmy Purewal <semmypurewal@gmail.com>
2013-06-01 12:40:32 -0400

    Add test text route

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.configure(function () {
     app.use(express.bodyParser());
 });
 
+app.get("/", function (req, res) {
+    res.send("hello world!");
+});
+
 app.get("/hello", function (req, res) {
     res.send("hello world!");
+});
+
+app.post("/text", function (req, res) {
+    console.log(req.body);
 });
 
